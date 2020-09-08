c0721661871527be8c1bc76826970ec10710f3c6
WooWan <woohobi@gmail.com>
2020-09-08 13:54:46 +0900

    correcting error in modal

--- modal/modal.js
+++ modal/modal.js
@@ -1,19 +1,18 @@
+const open_btn = document.querySelector(".open_btn");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const close_btn = document.querySelector(".close_btn");
+const content = document.querySelector(".content");
 
-const open_btn=document.querySelector(".open_btn");
-const modal=document.querySelector(".modal");
-const overlay=document.querySelector(".overlay");
-const close_btn=document.querySelector(".close_btn");
-const content=document.querySelector(".content");
-
-const open=() =>{
+const open = () => {
   overlay.classList.toggle("hidden");
   content.classList.toggle("active");
-}
+};
 const close = () => {
   overlay.classList.toggle("hidden");
   content.classList.toggle("active");
 };
 
 open_btn.addEventListener("click", open);
-overlay.addEventListener("click",close);
-close_btn.addEventListener("click",close);
+overlay.addEventListener("click", close);
+close_btn.addEventListener("click", close);
